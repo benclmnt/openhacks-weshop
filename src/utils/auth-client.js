5a5c41f30a720fb39d9791ed6b392da6b8498b97
Bennett Clement <benclmnt.dev@gmail.com>
2020-05-24 03:51:45 +0800

    Add utils

---
+++ src/utils/auth-client.js
@@ -0,0 +1,61 @@
+import { client, LOCAL_STORAGE_KEY } from './client';
+import {
+  API_LOGIN,
+  API_REGISTER
+} from '../constants/api-url';
+import { LOGIN } from '../constants/url';
+
+function handleUserResponse({ token, ...user }) {
+  window.localStorage.setItem(LOCAL_STORAGE_KEY, token);
+
+  // redirect user to homepage
+  window.location.replace('/');
+  return user;
+}
+
+// return either a user object or null
+function getUser() {
+  const token = getToken();
+  if (!token) {
+    return Promise.resolve(null);
+  }
+  return Promise.resolve({});
+  // return client(API_WHOAMI).then(data => data.user);
+}
+
+function login({ email, password }) {
+  return client(API_LOGIN, { body: { user: { email, password } } }).then(
+    handleUserResponse
+  );
+}
+
+function register({ name, email, password }) {
+  return client(API_REGISTER, {
+    body: { user: { name, email, password } },
+    redirectTo: LOGIN
+  })
+}
+
+function logout() {
+  window.localStorage.removeItem(LOCAL_STORAGE_KEY);
+
+  // refreshes the page for the user
+  window.location.assign(window.location);
+}
+
+function getToken() {
+  return window.localStorage.getItem(LOCAL_STORAGE_KEY);
+}
+
+function isLoggedIn() {
+  return Boolean(getToken());
+}
+
+export {
+  login,
+  register,
+  logout,
+  getToken,
+  getUser,
+  isLoggedIn,
+};
---
+++ src/utils/client.js
@@ -0,0 +1,84 @@
+import M from 'materialize-css';
+import { LOGIN } from '../constants/url';
+import { logout } from './auth-client';
+
+export const LOCAL_STORAGE_KEY = 'token';
+
+// customConfig params
+// - headers
+// - method
+// - body : will be JSONified.
+// - file (There is some unique things about files)!
+// - showSuccess : boolean
+// - showError : boolean
+// - onSuccess : fn
+// - redirecTo: string (starting with /)
+
+export async function client(endpoint, { body, file, ...customConfig } = {}) {
+  const token = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+  const headers = { 'Content-Type': 'application/json' };
+
+  if (token) {
+    headers.Authorization = `${token}`;
+  }
+
+  const config = {
+    method: customConfig.method ?? (body || file ? 'POST' : 'GET'),
+    headers: {
+      ...headers,
+      ...customConfig.headers
+    }
+  };
+
+  if (file) {
+    config.body = file;
+    delete config.headers['Content-Type'];
+  }
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  return await fetch(`/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      logout();
+      // TODO: shoule we refresh the page for them or redirect to login page ???
+      // window.location.assign(window.location);
+      window.location.assign(LOGIN);
+      return Promise.reject({ message: 'Please re-authenticate.' });
+    }
+
+    const data = await response.json();
+
+    if (response.ok) {
+      if (customConfig.showSuccess) {
+        M.toast({
+          html: `<div>${data.message}</div>`,
+          classes: 'teal rounded center top'
+        });
+      }
+
+      if (
+        customConfig.onSuccess &&
+        typeof customConfig.onSuccess === 'function'
+      ) {
+        customConfig.onSuccess();
+      }
+
+      if (customConfig.redirectTo) {
+        window.location.assign(customConfig.redirectTo);
+        return;
+      }
+
+      return data?.message;
+    } else {
+      if (customConfig.showError) {
+        M.toast({
+          html: `<div>${data?.error?.message}!</div>`,
+          classes: 'red rounded center top'
+        });
+      }
+      return Promise.reject(data);
+    }
+  });
+}
