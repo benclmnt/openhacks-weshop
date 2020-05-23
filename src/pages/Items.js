4ec4c42e119d6637c22100c5c4ccedbc12388c39
Bennett Clement <benclmnt.dev@gmail.com>
2020-05-24 06:17:17 +0800

    Try store with mocked data

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-function Items() {
+import '../css/image.css'
+import '../css/storeItems.css'
+
+function Item({ item }) {
   return (
-    <div>
-      
+    <div className="col s12 m6 l4 card z-depth-0" style={{ height: 350}}>
+      <div className="card-image" style={{ borderRadius: 5, boxShadow: '1px 3px 10px #aaa' }}>
+        {/* <div className="image-content"> */}
+        <img src={item.imageUrl}/>
+        {/* </div> */}
+        <span className="card-title">{item.name}</span>
+        <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="fas fa-plus" /></a>
+      </div>
+      <div className="card-content">
+        <p>{item.description}</p>
+      </div>
     </div>
   )
 }
 
-export default Items
+export default Item
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import { client } from '../utils/client';
+import data from './storeItems.json';
+import Items from './Items';
 
 const target =  "https://us-central1-aiot-fit-xlab.cloudfunctions.net/weshopgetcataloguebystoreid";
 
@@ -11,18 +13,24 @@ function Store() {
     client(target, {
       body: {
         storeid: "ad73c946-7ffb-4fea-9ede-721ed152d275"
-      }, 
-      headers: {
-        "Postman-Token": "fff88a62-18ec-42df-ac15-2204c32d3e63"
       }
     }).then(data => console.log(data))
   }, [])
-  
-  return (
-    <div>
-      Hello from store.
+
+  console.log(data);
+
+  const itemsList = data.map((item) => <Items item={item}/>);
+  return data.length > 0 ? (
+    <div className="row container">
+      <h3>Walmart</h3>
+      { itemsList }
+    </div>
+  ) : (
+    <div className="container">
+      We currently have no data from this store.
     </div>
   )
 }
 
-export default Store
+export default Store;
+
